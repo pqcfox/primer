eee21075965a6cc4433d78c65b39c51456ae139a
Chandler <use.an.alias@gmail.com>
2015-05-15 18:04:46 -0700

    Implement trial division

--- primer/factor.py
+++ primer/factor.py
@@ -5,11 +5,15 @@ import math
 
 def trial(n, sieve=True):
     factors = collections.Counter() 
+    upper = int(math.sqrt(n))
     divisors = list(range(2, upper)) if sieve else eratosthenes(upper) 
 
     for d in divisors:
         while n % d == 0:
             n //= d
             factors[d] += 1
+
+    if n > 1: 
+        factors[n] += 1
 
     return factors
